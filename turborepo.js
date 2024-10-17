977aa6299c193fe3c747adbd3c09baaaab9177b9
Jakob Lierman <jakob.lierman@gmail.com>
2024-10-17 17:03:59 +0200

    docs: add flat config todo

--- turborepo.js
+++ turborepo.js
@@ -5,5 +5,13 @@ module.exports = {
   extends: ['turbo'],
   rules: {
     'turbo/no-undeclared-env-vars': ['error', { allowList: ['CI', 'TZ'] }],
+    // 'import/no-extraneous-dependencies': [
+    //   'error',
+    //   {
+    //     packageDir: [
+    //       // TODO: Add package directories for monorepos
+    //     ]
+    //   }
+    // ],
   },
 };
