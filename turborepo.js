85b99d595c02e6b4402d3fd1e5572518caf71c61
Jakob Lierman <jakob.lierman@gmail.com>
2025-03-02 22:30:33 +0100

    fix: turborepo configuration upgrade

--- turborepo.js
+++ turborepo.js
@@ -6,7 +6,7 @@ const baseRule = baseRules['import/no-extraneous-dependencies'];
  * @type {import("eslint").Linter.Config}
  */
 module.exports = {
-  extends: ['turbo'],
+  extends: ['plugin:turbo/recommended'], // https://github.com/vercel/turborepo/pull/9978/files#r1974406494
   rules: {
     'turbo/no-undeclared-env-vars': ['error', { allowList: ['CI', 'TZ'] }],
     'import/no-extraneous-dependencies': [
