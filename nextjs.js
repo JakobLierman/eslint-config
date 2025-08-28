c2759f1d7f4378c584579eda591c4e9c303605a3
Jakob Lierman <jakob.lierman@gmail.com>
2025-08-28 11:25:13 +0200

    fix: nextjs typescript

--- nextjs.js
+++ nextjs.js
@@ -2,7 +2,7 @@
  * @type {import("eslint").Linter.Config}
  */
 module.exports = {
-  extends: ['./react.js', 'next/core-web-vitals'],
+  extends: ['./react.js', 'next/core-web-vitals', 'next/typescript'],
   rules: {
     'unicorn/prefer-node-protocol': 'off',
   },
