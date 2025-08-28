a5bab330be9c441eed0d9d8060b327e786369588
Jakob Lierman <jakob.lierman@gmail.com>
2025-08-28 11:31:45 +0200

    revert: nextjs typescript
    This reverts commit c2759f1d7f4378c584579eda591c4e9c303605a3.

--- nextjs.js
+++ nextjs.js
@@ -2,7 +2,7 @@
  * @type {import("eslint").Linter.Config}
  */
 module.exports = {
-  extends: ['./react.js', 'next/core-web-vitals', 'next/typescript'],
+  extends: ['./react.js', 'next/core-web-vitals'],
   rules: {
     'unicorn/prefer-node-protocol': 'off',
   },
